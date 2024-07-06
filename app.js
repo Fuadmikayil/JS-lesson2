55ef04280a1b46fe9e6f41a2bd23f7d032958c8b
Fuad <mikayilov2006@gmail.com>
2024-07-06 13:04:04 +0400

    and or

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 let  isDarkMode = true, isMarry = false, isPassed, hasCar = true
 const userId = "12345zxcvb",userGlobalId = "demo123"
 console.log(userId)
+///////////////////////////////
+let a = 10 , b=25,c=30;
+let z = a>b && console.log("salam");
+let d = a<b  || console.log("salam");
