6ad810f6cdc4fcbac980c21da1b51a168ecae692
Fuad <mikayilov2006@gmail.com>
2024-07-06 13:04:21 +0400

    if else

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ console.log(userId)
 let a = 10 , b=25,c=30;
 let z = a>b && console.log("salam");
 let d = a<b  || console.log("salam");
+
+///////////////////////////////////
+let login = "zaza123", pass = "777";
+const result = login === "zaza12345" && pass === "777"
+console.log(result ? "Welcome" : "Try again")
+result ? console.log("Welcome") : console.log("Try again")
