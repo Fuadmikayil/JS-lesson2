9f2d9cbc3e0ef5fc2d13ba40cb62d862e9397a84
Fuad <mikayilov2006@gmail.com>
2024-07-06 13:09:54 +0400

    string to number

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-let  isDarkMode = true, isMarry = false, isPassed, hasCar = true
-const userId = "12345zxcvb",userGlobalId = "demo123"
+let isDarkMode = true, isMarry = false, isPassed, hasCar = true
+const userId = "12345zxcvb", userGlobalId = "demo123"
 console.log(userId)
 ///////////////////////////////
-let a = 10 , b=25,c=30;
-let z = a>b && console.log("salam");
-let d = a<b  || console.log("salam");
+let a = 10, b = 25, c = 30;
+let z = a > b && console.log("salam");
+let d = a < b || console.log("salam");
 
 ///////////////////////////////////
 let login = "zaza123", pass = "777";
@@ -12,15 +12,21 @@ const result = login === "zaza12345" && pass === "777"
 console.log(result ? "Welcome" : "Try again")
 result ? console.log("Welcome") : console.log("Try again")
 ///////////////////////////
-let num= 10
+let num = 10
 
 num > 0
     ? console.log("Num is positive")
-    : num !=0
+    : num != 0
         ? console.log("Num is Negative")
-        :console.log("Num is zero")
+        : console.log("Num is zero")
 
 ///////////
 alert("Welcome To Facbook");
 let userName = prompt("Enter your name...")
 alert(`"Welcome ${userName}`)
+//////////////////////
+let v = 5
+///////stribi numbere çevirmek
+//Numer(a)
+//(+a) 
+console.log(+v)
